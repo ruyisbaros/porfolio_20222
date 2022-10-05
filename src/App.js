1f216041068b41d304d11f6c7f85508a9cf8bced
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-05 18:39:02 +0200

    some beginning refactors done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Project from "./pages/Project";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
